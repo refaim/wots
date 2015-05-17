05588eee78ae3865dfcdeeb71023d07a002624ae
Roman Kharitonov <refaim.vl@gmail.com>
2015-05-17 15:51:58 +1000

    Добавил механизм сигнализации о готовности конвертера валют к работе

--- src/core/currency.py
+++ src/core/currency.py
@@ -19,16 +19,17 @@ FORMAT_STRINGS = {
 
 class Converter(object):
     def __init__(self):
+        self.readyEvent = threading.Event()
         self.updateThread = None
         self.exchangeRates = {}
 
     def update(self):
         if self.updateThread is None or not self.updateThread.is_alive():
-            self.updateThread = threading.Thread(name='Currency', target=self._update, args=(self.exchangeRates,))
+            self.updateThread = threading.Thread(name='Currency', target=self._update, args=(self.exchangeRates, self.readyEvent,))
             self.updateThread.daemon = True
             self.updateThread.start()
 
-    def _update(self, results):
+    def _update(self, results, readyEvent):
         tree = lxml.etree.fromstring(network.getUrl('http://www.cbr.ru/scripts/XML_daily.asp'))
         for currency in tree.xpath('/ValCurs/Valute'):
             code = currency.xpath('CharCode')[0].text
@@ -36,11 +37,15 @@ class Converter(object):
             value = decimal.Decimal(currency.xpath('Value')[0].text.replace(',', '.'))
             results[code] = (nominal, value,)
         results[RUR] = (1, 1,)
+        readyEvent.set()
 
     def convert(self, srcCurrencyCode, dstCurrencyCode, amount):
         srcNominal, srcNominalValueInRoubles = self.exchangeRates[srcCurrencyCode]
         dstNominal, dstNominalValueInRoubles = self.exchangeRates[dstCurrencyCode]
         return decimal.Decimal(amount) / srcNominal * srcNominalValueInRoubles / dstNominalValueInRoubles * dstNominal
+
+    def isReady(self):
+        return self.readyEvent.isSet()
 
 
 def roundPrice(price):
