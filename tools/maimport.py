cb82c919adbf60e40f21e25c668749ed11c9f397
Roman Kharitonov <refaim.vl@gmail.com>
2015-08-01 20:22:46 +1000

    Обновил базу карт, переехал на парсинг нормального CSV при импорте из Magic Album

--- tools/maimport.py
+++ tools/maimport.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 import codecs
-import itertools
+import csv
 import json
 import os
 import sys
@@ -10,24 +10,40 @@ sys.path.append(os.path.abspath(os.path.join('..', 'src')))
 import wizard
 
 
-def main(args):
-    english = codecs.open('en', 'r', 'utf-8').read().splitlines()
-    russian = codecs.open('ru', 'r', 'utf-8').read().splitlines()
-    if len(english) != len(russian):
-        raise Exception('Length mismatch')
+def utf_8_encoder(unicode_csv_data):
+    for line in unicode_csv_data:
+        yield line.encode('utf-8')
 
-    cards = {}
-    for en, ru in itertools.izip(english, russian):
-        en = en.replace(u'│', u' / ')
-        ru = ru.replace(u'│', u' / ')
-        for cardname in (en, ru):
-            key = wizard.getCardCompletionKey(cardname)
-            if key:
-                values = cards.setdefault(key, [])
-                if not en in values:
-                    values.append(en)
-    with codecs.open('../resources/autocomplete.json', 'w', 'utf-8') as output:
-        output.write(json.dumps(cards, ensure_ascii=False, encoding='utf-8'))
+
+def unicode_csv_reader(unicode_csv_data, dialect=csv.excel, **kwargs):
+    csv_reader = csv.DictReader(utf_8_encoder(unicode_csv_data), dialect=dialect, **kwargs)
+    for row in csv_reader:
+        result = {}
+        for title, cell in row.iteritems():
+            if isinstance(cell, list) and len(cell) == 1:
+                cell = cell[0]
+            result[title] = unicode(cell, 'utf-8')
+        yield result
+
+
+def main(args):
+    autocomplete = {}
+
+    nameSeparator = u'\u2502'
+    with codecs.open(args[0], encoding='utf_16_le') as fp:
+        fp.readline()  # skip header
+        reader = unicode_csv_reader(fp, dialect=csv.excel_tab, fieldnames=['set', 'name', 'original', 'lang', 'number'])
+        for row in reader:
+            row['name'] = row['name'].replace(nameSeparator, u'|')
+            row['original'] = row['original'].replace(nameSeparator, u'|')
+            for key in (wizard.getCardCompletionKey(card) for card in (row['name'], row['original']) if row['lang'] in ('RUS', 'ENG')):
+                values = autocomplete.setdefault(key, [])
+                if not row['name'] in values:
+                    values.append(row['name'])
+
+    del autocomplete['']
+    with codecs.open('autocomplete.json', 'w', 'utf-8') as output:
+        output.write(json.dumps(autocomplete, ensure_ascii=False, encoding='utf-8'))
 
     return 0
 
