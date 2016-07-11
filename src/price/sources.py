f78a518856b6d08e1b79d8815c4f149503d780b2
Roman Kharitonov <refaim.vl@gmail.com>
2016-07-11 16:12:39 +1000

    Починил получение цен на нефойловые карты с TcgPlayer

--- src/price/sources.py
+++ src/price/sources.py
@@ -1,4 +1,5 @@
 import decimal
+import functools
 import lxml.html
 import queue
 import re
@@ -15,6 +16,7 @@ import card.sets
 import card.utils
 import core.currency
 import core.language
+import core.logger
 import core.network
 
 
@@ -79,75 +81,107 @@ def tcgProcessRequests(priceRequests, priceResults, setsRequests, setsResults, s
                     del cardsDict[cardKey]
 
 
-def tcgObtainSets(requests, results, exitEvent):
-    setQueryUrlTemplate = 'http://shop.tcgplayer.com/price-guide/magic/{}'
-    setsPrices = {}
-    app = QtWidgets.QApplication(sys.argv)
+def tcgObtainHtml(requests, results, exitEvent, logger):
+    application = QtWidgets.QApplication(sys.argv)
     browser = QtWebKitWidgets.QWebView()
-    app.exec_()
+    browserLock = threading.Lock()
+    browserTimer = QtCore.QTimer()
+    browserTimer.timeout.connect(functools.partial(tcgObtainHtmlWaitData, browser, browserLock, results, exitEvent, logger))
+    browserTimer.start(100)
+    workTimer = QtCore.QTimer()
+    workTimer.timeout.connect(functools.partial(tcgObtainHtmlProcessRequests, workTimer, browser, browserLock, requests, exitEvent, logger))
+    workTimer.start(100)
+    application.exec_()
+
+
+def tcgObtainHtmlProcessRequests(timer, browser, browserLock, requests, exitEvent, logger):
+    if exitEvent.is_set():
+        QtWidgets.QApplication.quit()
+        # TODO stop timer?
+        # TODO send signal to application object
+
+    if not browserLock.acquire(blocking=False):
+        return
+
+    try:
+        qualifiedUrl = requests.get_nowait()
+    except queue.Empty:
+        browserLock.release()
+        return
+
+    logger.info('Loading [GET] {}'.format(qualifiedUrl))
+    browser.load(QtCore.QUrl(qualifiedUrl))
+
+
+def tcgObtainHtmlWaitData(browser, browserLock, results, exitEvent, logger):
+    if exitEvent.is_set():
+        QtWidgets.QApplication.quit()
+        # TODO stop timer?
+        # TODO send signal to application object
+
+    htmlString = browser.page().mainFrame().toHtml()
+    if len(htmlString) > 30 * 1024:
+        results.put(htmlString)
+        logger.info('Finished [GET] {}'.format(str(browser.url())))
+        browser.setUrl(QtCore.QUrl(''))
+        browserLock.release()
+
+
+def tcgObtainSets(priceRequests, priceResults, htmlRequests, htmlResults, exitEvent, logger):
+    setsPrices = {}
+    setQueryUrlTemplate = 'http://shop.tcgplayer.com/price-guide/magic/{}'
     while True:
         if exitEvent.is_set():
             return
 
-        setFullName = requests.get()
-        setAbbrv = card.sets.tryGetAbbreviation(setFullName)
-        if setAbbrv not in setsPrices:
-            browser.load(QtCore.QUrl(setQueryUrlTemplate.format(urllib.parse.quote(setFullName.lower()))))
-            print(browser.page())
-            exit()
-            # html = lxml.html.document_fromstring(core.network.getUrl(setQueryUrlTemplate.format(urllib.parse.quote(setFullName.lower()))))
+        htmlString = None
+        try:
+            htmlString = htmlResults.get_nowait()
+        except queue.Empty:
+            pass
+        if htmlString is not None:
+            html = lxml.html.document_fromstring(htmlString)
+            setNameString = re.match(r'Magic: The Gathering - (.+?)Price Guide', html.cssselect('title')[0].text).group(1).strip()
+            setAbbrv = card.sets.tryGetAbbreviation(setNameString)
             setsPrices[setAbbrv] = {}
             for row in html.cssselect('table.priceGuideTable tr'):
-                cardName = row.cssselect('td.product div.productDetail a')[0].text
-                print(cardName)
-                # cells = row.cssselect('td')
-                # if cells and len(cells) >= 5:
-                #     cellCardNameString = cells[0].cssselect('font')[0].text
-                #     if cellCardNameString.startswith('&nbsp;'):
-                #         cellCardNameString = cellCardNameString[6:]
-                #     cellCardKey = getCardKey(cellCardNameString, 'EN', False)
-                #     priceString = cells[6].cssselect('font')[0].text
-                #     if priceString and priceString.startswith('$'):
-                #         setsPrices[setAbbrv][cellCardKey] = {
-                #             'price': decimal.Decimal(re.match(r'\$([\d\.]+).*', priceString).group(1)),
-                #             'currency': core.currency.USD,
-                #         }
-            # tables = html.cssselect('table')
-            # if tables:
-            #     setsPrices[setAbbrv] = {}
-            #     for resultsEntry in tables[-1].cssselect('tr'):
-            #         cells = resultsEntry.cssselect('td')
-            #         if cells and len(cells) >= 5:
-            #             cellCardNameString = cells[0].cssselect('font')[0].text
-            #             if cellCardNameString.startswith('&nbsp;'):
-            #                 cellCardNameString = cellCardNameString[6:]
-            #             cellCardKey = getCardKey(cellCardNameString, 'EN', False)
-            #             priceString = cells[6].cssselect('font')[0].text
-            #             if priceString and priceString.startswith('$'):
-            #                 setsPrices[setAbbrv][cellCardKey] = {
-            #                     'price': decimal.Decimal(re.match(r'\$([\d\.]+).*', priceString).group(1)),
-            #                     'currency': core.currency.USD,
-            #                 }
-        results.put((setAbbrv, setsPrices[setAbbrv]))
+                cardUrls = row.cssselect('.productDetail a')
+                if cardUrls:
+                    cellCardKey = getCardKey(cardUrls[0].text, 'EN', False)
+                    priceString = row.cssselect('.medianPrice .cellWrapper')[0].text
+                    setsPrices[setAbbrv][cellCardKey] = {
+                        'price': decimal.Decimal(re.match(r'\s*\$([\d\.]+).*', priceString).group(1)),
+                        'currency': core.currency.USD,
+                    }
+            priceResults.put((setAbbrv, setsPrices[setAbbrv]))
+
+        try:
+            setFullName = priceRequests.get_nowait()
+        except queue.Empty:
+            continue
+        setAbbrv = card.sets.tryGetAbbreviation(setFullName)
+        if setAbbrv not in setsPrices:
+            qualifiedUrl = setQueryUrlTemplate.format(urllib.parse.quote(setFullName.lower()))
+            htmlRequests.put(qualifiedUrl)
 
 
-def tcgObtainSingles(requests, results, exitEvent):
-    cardQueryUrlTemplate = 'http://shop.tcgplayer.com/magic/{}/{}'
-    while True:
-        if exitEvent.is_set():
-            return
+# def tcgObtainSingles(requests, results, exitEvent):
+#     cardQueryUrlTemplate = 'http://shop.tcgplayer.com/magic/{}/{}'
+#     while True:
+#         if exitEvent.is_set():
+#             return
 
-        cardName, setName, cardLang = requests.get()
-        tcgSetName = tcgMakeUrlPart(setName)
-        tcgCardName = tcgMakeUrlPart(card.utils.escape(cardName))
-        priceHtml = lxml.html.document_fromstring(core.network.getUrl(cardQueryUrlTemplate.format(tcgSetName, tcgCardName)))
-        for priceBlock in priceHtml.cssselect('.priceGuideClass'):
-            foil = priceBlock.cssselect('.cardStyle b')[0].text == 'Foil'
-            priceInfo = {
-                'price': decimal.Decimal(re.match(r'.*?\$([\d\.]+).*', priceBlock.cssselect('.priceRange .median')[0].text).group(1)),
-                'currency': core.currency.USD,
-            }
-            results.put((card.sets.tryGetAbbreviation(setName), getCardKey(cardName, cardLang, foil), priceInfo,))
+#         cardName, setName, cardLang = requests.get()
+#         tcgSetName = tcgMakeUrlPart(setName)
+#         tcgCardName = tcgMakeUrlPart(card.utils.escape(cardName))
+#         priceHtml = lxml.html.document_fromstring(core.network.getUrl(cardQueryUrlTemplate.format(tcgSetName, tcgCardName)))
+#         for priceBlock in priceHtml.cssselect('.priceGuideClass'):
+#             foil = priceBlock.cssselect('.cardStyle b')[0].text == 'Foil'
+#             priceInfo = {
+#                 'price': decimal.Decimal(re.match(r'.*?\$([\d\.]+).*', priceBlock.cssselect('.priceRange .median')[0].text).group(1)),
+#                 'currency': core.currency.USD,
+#             }
+#             results.put((card.sets.tryGetAbbreviation(setName), getCardKey(cardName, cardLang, foil), priceInfo,))
 
 
 def tcgMakeUrlPart(rawString):
@@ -180,6 +214,7 @@ class TcgPlayer(object):
             'TST': 'Timeshifted',
         }
         self.priceResults = resultsQueue
+        self.logger = core.logger.Logger('TcgPlayer')
         self.restart()
 
     def getTitle(self):
@@ -203,16 +238,19 @@ class TcgPlayer(object):
         self.singlesResults = queue.Queue()
         self.setsRequests = queue.Queue()
         self.setsResults = queue.Queue()
+        self.htmlRequests = queue.Queue()
+        self.htmlResults = queue.Queue()
 
-        self.htmlObtainer = threading.Thread
+        self.htmlObtainer = threading.Thread(name='TCG-Html', target=tcgObtainHtml, args=(self.htmlRequests, self.htmlResults, self.exitEvent, self.logger,))
         # self.singlesObtainer = threading.Thread(name='TCG-Singles', target=tcgObtainSingles, args=(self.singlesRequests, self.singlesResults, self.exitEvent,), daemon=True)
-        self.setsObtainer = threading.Thread(name='TCG-Sets', target=tcgObtainSets, args=(self.setsRequests, self.setsResults, self.exitEvent,), daemon=True)
+        self.setsObtainer = threading.Thread(name='TCG-Sets', target=tcgObtainSets, args=(self.setsRequests, self.setsResults, self.htmlRequests, self.htmlResults, self.exitEvent, self.logger,), daemon=True)
         self.priceRequestsProcessor = threading.Thread(
             name='TCG-Main',
             target=tcgProcessRequests,
             args=(self.priceRequests, self.priceResults, self.setsRequests, self.setsResults, self.singlesRequests, self.singlesResults, self.exitEvent,),
             daemon=True)
         # self.singlesObtainer.start()
+        self.htmlObtainer.start()
         self.setsObtainer.start()
         self.priceRequestsProcessor.start()
 
