9cba5aa1399b6d8d06c047576ab223a8cbbbd779
Roman Kharitonov <rkharito@yandex.ru>
2018-07-10 23:58:24 +1000

    Encrypt Python bytecode when building executable

--- build.py
+++ build.py
@@ -3,11 +3,22 @@ import platform
 import sys
 
 import PyQt5
+import dotenv
+from PyInstaller.archive.pyz_crypto import PyiBlockCipher
 from PyInstaller.building.api import PYZ, EXE, COLLECT
 from PyInstaller.building.build_main import Analysis
 
 RESOURCES_DIRECTORY = 'res'
 BINARY_RESOURCE_EXTENSIONS = {'.png'}
+
+
+class BytesPyiBlockCipher(PyiBlockCipher):
+    def __init__(self, key: bytes):
+        super().__init__('')
+        self.key = key
+
+
+dotenv.load_dotenv('.env')
 
 path = []
 if platform.system() == 'Windows':
@@ -25,11 +36,15 @@ for filename in os.listdir(RESOURCES_DIRECTORY):
         target = bin_resources
     target.append((os.path.join(RESOURCES_DIRECTORY, filename), RESOURCES_DIRECTORY))
 
-block_cipher = None
+cipher_key = os.getenv('PYINSTALLER_CIPHER_KEY')
+if cipher_key is not None:
+    cipher_key = cipher_key.encode('utf-8')
+block_cipher = BytesPyiBlockCipher(key=cipher_key)
+
 a = Analysis([os.path.join('app', 'wizard.py')],
              pathex=path, binaries=bin_resources, datas=txt_resources, hiddenimports=[], hookspath=[],
              runtime_hooks=[], excludes=[], win_no_prefer_redirects=False, win_private_assemblies=False,
              cipher=block_cipher)
 pyz = PYZ(a.pure, a.zipped_data, cipher=block_cipher)
 exe = EXE(pyz, a.scripts, exclude_binaries=True, name='wizard', debug=False, strip=False, upx=False, console=False)
-coll = COLLECT(exe, a.binaries, a.zipfiles, a.datas, strip=False, upx=False, name='Wizard of the Search')
+COLLECT(exe, a.binaries, a.zipfiles, a.datas, strip=False, upx=False, name=os.getenv('APP_NAME'))
